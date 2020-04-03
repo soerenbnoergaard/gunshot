3fef152276ad356fc45bc76102b4f25ba292d025
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2020-04-03 19:55:26 +0200

    Stored all plugin state in a struct

--- src/gunshot/test/test.cpp
+++ src/gunshot/test/test.cpp
@@ -10,27 +10,80 @@
 #define BUFFER_SIZE 128
 #define NUM_TEST_SAMPLES (BUFFER_SIZE*2400)
 
+// Plugin state
+typedef struct {
+    float *ir_left;
+    float *ir_right;
+    uint32_t ir_sample_rate_Hz;
+    uint32_t ir_num_channels;
+    uint32_t ir_num_samples_per_channel;
+    uint32_t ir_bit_depth;
+} plugin_state_t;
+
 float get_sample(uint32_t n, uint32_t sample_rate_Hz)
 {
     return 0.01*sin(1000.0 * n / sample_rate_Hz);
 }
 
+int init_plugin_state(plugin_state_t *state, const char *filename)
+{
+    int n;
+    AudioFile<float> ir;
+
+    ir.load(filename);
+
+    if ((ir.getNumChannels() < 1) || (2 < ir.getNumChannels())) {
+        return 1;
+    }
+
+    state->ir_left = NULL;
+    state->ir_left = (float *)malloc(sizeof(float)*ir.getNumSamplesPerChannel());
+    if (state->ir_left == NULL) {
+        return 1;
+    }
+
+    state->ir_right = NULL;
+    state->ir_right = (float *)malloc(sizeof(float)*ir.getNumSamplesPerChannel());
+    if (state->ir_right == NULL) {
+        return 1;
+    }
+
+    for (n = 0; n < ir.getNumSamplesPerChannel(); n++) {
+        state->ir_left[n] = ir.samples[0][n];
+        if (ir.getNumChannels() > 1) {
+            state->ir_right[n] = ir.samples[1][n];
+        }
+    }
+
+    state->ir_num_samples_per_channel = ir.getNumSamplesPerChannel();
+    state->ir_num_channels = ir.getNumChannels();
+    state->ir_sample_rate_Hz = ir.getSampleRate();
+    state->ir_bit_depth = ir.getBitDepth();
+
+    return 0;
+}
+
 int main(void)
 {
-    AudioFile<float> ir;
+    int err;
     fftconvolver::FFTConvolver convolver;
-    uint32_t N;
     float y[NUM_TEST_SAMPLES];
     float x[NUM_TEST_SAMPLES];
 
-    // Load impulse response from file
-    ir.load("test.wav");
-    ir.printSummary();
-    convolver.init(FFT_BLOCK_SIZE, (fftconvolver::Sample *)(&ir.samples[0][0]), ir.getNumSamplesPerChannel());
+    // Initialize plugin state
+    plugin_state_t state;
+    err = init_plugin_state(&state, "test.wav");
+    if (err) {
+        printf("Got error\n");
+        return 1;
+    }
+
+    // Initialize convolution kernel
+    convolver.init(FFT_BLOCK_SIZE, (fftconvolver::Sample *)state.ir_left, state.ir_num_samples_per_channel);
 
     // Generate test data
     for (int n = 0; n < 24000; n++) {
-        x[n] = get_sample(n, ir.getSampleRate());
+        x[n] = get_sample(n, state.ir_sample_rate_Hz);
     }
 
     // Run convolution
@@ -52,8 +105,8 @@ int main(void)
     }
 
     out.setNumChannels(1);
-    out.setBitDepth(ir.getBitDepth());
-    out.setSampleRate(ir.getSampleRate());
+    out.setBitDepth(state.ir_bit_depth);
+    out.setSampleRate(state.ir_sample_rate_Hz);
     bool ok = out.setAudioBuffer(out_buffer);
     out.save("out.wav");
     printf("OK? %d\n", ok);
