57a683f8b61b9d8fc42e686b8eedecef320db62e
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2020-04-03 18:07:33 +0200

    Added AudioFile lib to take care of WAW IO

--- src/gunshot/test/test.cpp
+++ src/gunshot/test/test.cpp
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
-#include "../utils.h"
+#include "AudioFile.h"
 
 int main(void)
 {
-    status_t err;
-    float *left = NULL;
-    float *right = NULL;
-    uint32_t length = 0;
-    uint32_t sample_rate_Hz = 0;
-
-    err = load("test.wav", left, right, &length, &sample_rate_Hz);
-    if (err != STATUS_OK) {
-        printf("Failed to load test.wav\n");
-    }
+    AudioFile<float> wav;
+    wav.load("test.wav");
+    wav.printSummary();
+    return 0;
 }
