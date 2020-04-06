f99e7ed097dd908fb119d4725eeef052ea17f3c5
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2020-04-06 16:04:45 +0200

    Debugging windows crash

--- src/gunshot/GunShot.cpp
+++ src/gunshot/GunShot.cpp
@@ -204,6 +204,8 @@ protected:
      */
     String getState(const char* key) const override
     {
+        log_write("getState called");
+
         // Return the cached version of `state` instead of re-serializing it.
         if (std::strcmp(key, "state") == 0) {
             return state_cache;
